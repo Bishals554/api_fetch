52e6a30f05ebce43dd2e1725b466d79cc57225ba
Bishal Shrestha <bishals554@gmaul.com>
2024-02-24 13:46:29 +0545

    added sort

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -11,14 +11,25 @@ class ApiService {
   String url = 'https://dummy.restapiexample.com/api/v1/employees';
 
   Future<DataModel?> getData() async {
-    final response = await dio.get(url);
-    print(response.data);
-    print(response.statusCode);
+    try {
+      final response = await dio.get(url);
+      print(response.data);
+      print(response.statusCode);
 
-    if (response.statusCode == HttpStatus.ok) {
-      return DataModel.fromJson(response.data);
-    } else {
-      return null;
+      if (response.statusCode == HttpStatus.ok) {
+        return DataModel.fromJson(response.data);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      if (e is DioException &&
+          e.response?.statusCode == HttpStatus.tooManyRequests) {
+        // Retry after a short delay
+        await Future.delayed(Duration(seconds: 5));
+      } else {
+        rethrow; // Rethrow the exception if it's not a 429 error
+      }
     }
+    return null;
   }
 }
